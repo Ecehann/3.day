8992aabba0f3f08144906811071f797ea2e39a12
Ecehan Gülmez <113697871+Ecehann@users.noreply.github.com>
2022-10-24 18:24:20 +0300

    Add files via upload

---
+++ inheritance/src/inheritance/CustomerManager.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class CustomerManager extends PersonManager  {
+	
+	/* 	public void List() {
+			System.out.println("Listelendi.");
+		}
+	
+		public void Add() {
+			System.out.println("Eklendi.");
+		} */
+	
+	
+}
